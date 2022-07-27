9a448edbdb96d7983ab210fe801ff0a9232b7a2e
Ds_mohamed <smdioubate87@yahoo.com>
2022-07-27 23:35:35 +0200

    add swagger

--- src/main/java/com/mediscreen_note/MediscreenNoteApplication.java
+++ src/main/java/com/mediscreen_note/MediscreenNoteApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+
+@EnableSwagger2 
 public class MediscreenNoteApplication  {
 	 @Bean
 	    public ModelMapper modelMapper() {
